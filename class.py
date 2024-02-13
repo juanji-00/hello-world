ddacb40620bf70180ecbcfa1d5c685ff47615196
juanji-00 <155633769+juanji-00@users.noreply.github.com>
2024-02-13 11:24:54 +0100

    Add files via upload

---
+++ class.py
@@ -0,0 +1,34 @@
+# Creación de una clase
+
+class Dog:
+
+    species = "Canis lupus familiaris"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+    
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+    
+
+perro = Dog("Pepe", 12)
+
+print(perro.speak("Woou!"))
+
+# Herencia de una clase
+
+class GoldenRetriever(Dog):
+    # Valor por defecto en .speak(), definimos sound y utilizamos super().
+    def speak(self, sound="Bark"):
+        return super().speak(sound)
+    
+
+buddy = GoldenRetriever("Pepi", 45)
+
+print(buddy.speak())
+
+
